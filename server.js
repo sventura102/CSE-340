5f72c4b01a8a2d6cb91e8c3ce9bfecaabf7a5d15
Stephanie <sventura102@gmail.com>
2023-11-16 21:17:19 -0800

    activity

--- server.js
+++ server.js
@@ -7,6 +7,7 @@
  *************************/
 const inventoryRoute = require("./routes/inventoryRoute")
 const baseController = require("./controllers/baseController")
+const utilities = require("./utilities/")
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
@@ -27,13 +28,29 @@ app.set("layout", "./layouts/layout") // not a views root
  *************************/
 app.use(require("./routes/static"))
 //Index route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", inventoryRoute)
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
+
 
 /* ***********************
  * Local Server Information
@@ -49,16 +66,3 @@ app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
 
-/* ***********************
-* Express Error Handler
-* Place after all other middleware
-*************************/
-app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav()
-  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  res.render("errors/error", {
-    title: err.status || 'Server Error',
-    message: err.message,
-    nav
-  })
-})
--- utilities/index.js
+++ utilities/index.js
@@ -24,6 +24,14 @@ Util.getNav = async function (req, res, next) {
   return list
 }
 
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
+
+
 module.exports = Util
 
 
@@ -59,3 +67,4 @@ Util.buildClassificationGrid = async function(data){
   }
   return grid
 }
+

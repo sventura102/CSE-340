19f545ce74ee686f413f210b4e6f3114082e7d65
Stephanie <sventura102@gmail.com>
2023-12-05 21:41:16 -0800

    html val and route fixed

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -15,19 +15,10 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
 router.get("/registration", utilities.handleErrors(accountController.buildRegistration))
 
 // Process registration data
-router.post("/registration",
-    regValidate.registationRules(),
-    regValidate.checkRegData,
-    utilities.handleErrors(accountController.registerAccount)
-)
+router.post("/registration", regValidate.registationRules(), regValidate.checkRegData, utilities.handleErrors(accountController.registerAccount))
 
 // Process the login attempt
-router.post(
-  "/login",
-  regValidate.loginRules(),
-  regValidate.checkLoginData,
-  utilities.handleErrors(accountController.accountLogin)
-)
+router.post("/login", regValidate.loginRules(), regValidate.checkLoginData, utilities.handleErrors(accountController.accountLogin))
 
 
 module.exports = router;

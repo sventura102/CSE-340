30aee1eb8b7eccdbc7dae6f7c570d7c917001b8f
Stephanie <sventura102@gmail.com>
2023-12-07 19:32:11 -0800

    added update and delete models

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -60,4 +60,25 @@ async function addVehicle(classification_id, inv_make, inv_model, inv_descriptio
     return error.message
   }
 }
-module.exports = {getClassifications, getInventoryByClassificationId, getInventoryByDetailId, addClassification, addVehicle}
+
+async function updateVehicle(inv_id, inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color, classification_id) {
+  try {
+    const sql = "UPDATE public.inventory SET inv_make = $1, inv_model = $2, inv_description = $3, inv_image = $4, inv_thumbnail = $5, inv_price = $6, inv_year = $7, inv_miles = $8, inv_color = $9, classification_id = $10 WHERE inv_id = $11 RETURNING *"
+    const data = await pool.query(sql, [inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color, classification_id, inv_id])
+    return data.rows[0]
+  } catch (error) {
+    console.error("model error: " + error)
+  }
+}
+
+async function deleteVehicle(inventory_id) {
+  try {
+    const sql = 'DELETE FROM public.inventory WHERE inv_id = $1'
+    const data = await pool.query(sql, [inventory_id])
+  return data
+  } catch (error) {
+    new Error("Delete Inventory Error")
+  }
+}
+
+module.exports = {getClassifications, getInventoryByClassificationId, getInventoryByDetailId, addClassification, addVehicle, updateVehicle, deleteVehicle}

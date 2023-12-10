30c0ec5a4b6c335ba93c81257ece119635f53dbf
Stephanie <sventura102@gmail.com>
2023-12-09 20:20:24 -0800

    created functions $ models assocaited w updating account and password

--- models/account-model.js
+++ models/account-model.js
@@ -51,4 +51,34 @@ async function getAccountByEmail (account_email) {
     return new Error("No matching email found")
   }
 }
-module.exports = {registerAccount, checkExistingEmail, loginAccount, getAccountByEmail}
+
+async function getAccountById (account_id) {
+  try {
+    const result = await pool.query(
+      'SELECT account_id, account_firstname, account_lastname, account_email, account_type, account_password FROM account WHERE account_id = $1',
+      [account_id])
+    return result.rows[0]
+  } catch (error) {
+    return new Error("No matching id found")
+  }
+}
+async function updateAccount (account_id, account_firstname, account_lastname, account_email) {
+  try {
+    const sql = "UPDATE public.account SET account_firstname = $1, account_lastname = $2, account_email = $3 WHERE account_id = $4 RETURNING *"
+    const data = await pool.query(sql, [account_firstname, account_lastname, account_email, account_id])
+    return data.rows[0]
+  } catch (error) {
+    console.error("account error: " + error)
+  }
+}
+
+async function updatePassword(account_id, account_password) {
+  try {
+    const sql = "UPDATE account SET account_password = $2 WHERE account_id = $1 RETURNING *"
+    const data = await pool.query(sql, [account_password, account_id])
+    return data.rows[0]
+  } catch (errors) {
+    console.error("Update Password Error")
+  }
+}
+module.exports = {registerAccount, checkExistingEmail, loginAccount, getAccountByEmail, getAccountById, updateAccount, updatePassword}
